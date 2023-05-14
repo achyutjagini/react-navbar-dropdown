f32a587acbc1f84f8de1d769528aadbaecb4d2b1
achyutjagini <achyut.jagini@gmail.com>
2023-05-14 16:55:37 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import ContactUs from './components/pages/ContactUs';
+import SignUp from './components/pages/SignUp';
+import Marketing from './components/pages/Marketing';
+import Consulting from './components/pages/Consulting';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <Navbar />
-      </BrowserRouter>
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/contact-us' element={<ContactUs />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/marketing' element={<Marketing />} />
+          <Route path='/consulting' element={<Consulting />} />
+        </Routes>
+      </Router>
     </>
 
   );
